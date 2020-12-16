3d288c0678f39f940238f6a6ec048aab111d44cd
Frank Kang <kanchy@gmail.com>
2020-12-16 10:40:06 +0800

    update

--- statcan/statcan.py
+++ statcan/statcan.py
@@ -2,6 +2,10 @@ import pandas as pd
 import geopandas as gpd
 import plotly.express as px
 import json
+import logging
+
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 flist = ('98-401-X2016059_English_CSV_data.csv',  # Canada, provinces and territories
         '98-401-X2016066_English_CSV_data.csv', # Census subdivisions (CSD) - Ontario only
@@ -97,45 +101,86 @@ class CensusData:
         out_data = out_data[out_data['Total Minority'].astype(float) != 0]
         out_data.to_csv(self.update_data_file, index=False)
 
-    def show(self):
-
+    def prune_map(self, mapdata, mapdata_key, dataframe, dataframe_key):
         
-        country_map = gpd.read_file(self.choroplethmap_file).to_crs("EPSG:4326")
-        #with open("test.json") as f:
-        #    country_map = json.load(f)
+        index_list = []
+        for index, row in mapdata.iterrows():
+            mkey = row[mapdata_key]
+            if mkey not in dataframe[dataframe_key].values.astype('str'):
+                i = mapdata.loc[mapdata[mapdata_key] == mkey].index.to_list()
+                assert(len(i) == 1)
+                index_list += i
+        pruned = mapdata.drop(index=index_list)
+        
+        print('Pruned: {} -> {}'.format(len(mapdata), len(pruned)))
+        return pruned
 
-        df = pd.read_csv(self.update_data_file)
+    def generate_tip_data(self, dataframe):
         
         tip_data = {}
         color = None
-        for col in df.columns:
+        for col in dataframe.columns:
             if col in ['GEO_NAME', 'GEO_CODE', 'CSD_TYPE_NAME','Not a visible minority']:
                 tip_data[col] = False
             elif col in ['Total']:
                 tip_data[col] = ':,'
             elif col in ['Total Minority']:
                 tip_data[col] = ':,'
-                tip_data[col+' ratio'] = (':.2%',df[col].astype(float)/df['Total'])
+                tip_data[col+' ratio'] = (':.2%',dataframe[col].astype(float)/dataframe['Total'])
             else:
                 if 'Chinese' == col:
-                    color = df[col].astype(float)/df['Total Minority']
-                tip_data[col+' ratio'] = (':.2%',df[col].astype(float)/df['Total Minority'])
+                    color = dataframe[col].astype(float)/dataframe['Total Minority']
+                tip_data[col+' ratio'] = (':.2%',dataframe[col].astype(float)/dataframe['Total Minority'])
+
+        return tip_data
+
+    def show(self, threshold, filename='out.html'):
         
-        fig = px.choropleth_mapbox(df, geojson=country_map, featureidkey="properties.CSDUID", locations='GEO_CODE', color=color,
+        logger.info("Read map data.")
+        country_map = gpd.read_file(self.choroplethmap_file).to_crs("EPSG:4326")
+        #with open("test.json") as f:
+        #    country_map = json.load(f)
+
+        logger.info("Read population data.")
+        df = pd.read_csv(self.update_data_file)
+        
+        logger.info("Filter with threshold.")
+        df = df.loc[df['Total Minority'] >= threshold]
+        
+        logger.info("Generate tip data.")
+        tip_data = self.generate_tip_data(df)
+        
+        mapdata_key = 'CSDUID'
+        dataframe_key = 'GEO_CODE'
+        logger.info("Prune map data.")
+        country_map = self.prune_map(country_map, mapdata_key, df, dataframe_key)
+
+        logger.info("Generate figure with choropleth_mapbox.")
+        fig = px.choropleth_mapbox(df, geojson=country_map, featureidkey="properties.{}".format(mapdata_key), locations=dataframe_key, color=tip_data['Chinese ratio'][1],
                                    color_continuous_scale="sunsetdark",
-                                   mapbox_style="carto-positron",
+                                   #mapbox_style="carto-positron",
+                                   mapbox_style="open-street-map",
                                    center = {"lat": 43.0, "lon": -79.0},
                                    zoom=8,
                                    opacity=0.5,
                                    hover_name='GEO_NAME',
                                    hover_data=tip_data,
                                    template='ggplot2',
-                                   #labels={'color': 'Chinese'},
+                                   labels={'color': 'Chinese'},
                                   )
   
-        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        logger.info("Update layout.")
+        fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0}, title={"text":'Minorities in Canada with the threshold {}'.format(threshold),})
+        
+        if filename:
+            logger.info("Write html.")
+            fig.write_html(filename, include_plotlyjs=True)
+            print('Write to {}'.format(filename))
+
+        logger.info("Show it.")
         fig.show()
-        pass
+
+        logger.info("Done.")
     
     def show2(self):
         token = open(".mapbox_token").read() 
@@ -182,10 +227,11 @@ if __name__ == '__main__':
     
     parser = ArgumentParser(description='Minorities in Canada')
     parser.add_argument('-u','--update', action="store_true", help='Update data...', default=False)
+    parser.add_argument('-t','--threshold', help='Threshold for minority population...', type=int, default=1000)
     args = parser.parse_args()
 
     cd = CensusData()
     if args.update:
         cd.update_data()
     else:
-        cd.show()
+        cd.show(threshold=args.threshold)
